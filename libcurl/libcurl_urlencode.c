ccd7082d669c1f7b3a6d269c654a3307469746ae
june.kim <june.kim@kakaocorp.com>
2019-04-24 11:31:34 +0900

    minor fix

--- libcurl/libcurl_urlencode.c
+++ libcurl/libcurl_urlencode.c
@@ -6,11 +6,11 @@ int main() {
     CURL *curl = curl_easy_init();
     if (curl) {
         char *string = "hello world";
-        printf("%lu\n", strlen(string));
+        printf("%lu\n", strlen(string)); // 11
 
         char *output = curl_easy_escape(curl, string, strlen(string));
         if (output) {
-            printf("Encoded: %s\n", output);
+            printf("Encoded: %s\n", output); // Encoded: hello%20world
             curl_free(output);
         }
     }
