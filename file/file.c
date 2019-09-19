584adf8a29ada4706fba7a7f82ad2894440a1dde
june.kim <june.kim@kakaocorp.com>
2019-09-19 11:54:17 +0900

    rearrange

--- file/file.c
+++ file/file.c
@@ -33,15 +33,14 @@ void read_test() {
 
     fp = fopen(file_name, "r"); // read mode
 
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
     }
 
     printf("The contents of %s file are:\n", file_name);
 
-    while((ch = fgetc(fp)) != EOF)
+    while ((ch = fgetc(fp)) != EOF)
         printf("%c", ch);
 
     fclose(fp);
